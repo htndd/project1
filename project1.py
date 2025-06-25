4f59452702cd4ed061f6912d38ff1ce6d7ce989c
htndd <tonh6277@gmail.com>
2025-06-25 20:06:49 +0800

    Update project1.py
    成功調用api

--- project1.py
+++ project1.py
@@ -1,1 +1,20 @@
-print("666")
+import requests
+
+
+def get_crypto_price(crypto_id):
+    url = f'https://api.coingecko.com/api/v3/simple/price?ids={crypto_id}&vs_currencies=usd'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data[crypto_id]['usd']
+    else:
+        return None
+
+if __name__ == "__main__":
+    crypto_list = ['bitcoin', 'ethereum']
+    for crypto in crypto_list:
+        price = get_crypto_price(crypto)
+        if price:
+            print(f'{crypto.capitalize()} 價格：${price}')
+        else:
+            print(f'無法取得 {crypto} 價格')
